10953d7bfeaa5ab878f8218f803a50a79cb0aca6
Pritam Kumar <pritampriyam07@gmail.com>
2026-04-17 11:06:02 +0530

    Update frontend API for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: false,
-});
+const baseURL =
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:5000/api";
 
-export default API;
+export default axios.create({
+  baseURL,
+});
